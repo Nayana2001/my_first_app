74d7bc5fdcd0418282208fcaf4ad5d513c50e7ea
nayanashree.a <nayanashree.a2001@gmail.com>
2023-04-20 11:49:29 +0530

    updated- add ascii

--- my_first_app/main/src/com/nagra/MyApp.java
+++ my_first_app/main/src/com/nagra/MyApp.java
@@ -1,7 +1,53 @@
 package com.nagra;
 
-public class MyApp {
-    public static void main(String[] args) {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/*public class MyApp {
+    public static <TextWidget> void main(String[] args)  {
         System.out.println("Hello world! Nayanashree.A");
     }
+
+}*/
+public class MyApp {
+    public static void main(String[] args) {
+        try {
+            // Load the image file
+            File imageFile = new File("C:/Users/NayanaShree.A/my_first_app/my_first_app/src/main/resources/Images/IndiaApp.png");
+            BufferedImage image = ImageIO.read(imageFile);
+
+            // Resize the image to a smaller size for better ASCII representation
+            int newWidth = image.getWidth() / 8;
+            int newHeight = image.getHeight() / 8;
+            BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+            resizedImage.createGraphics().drawImage(image, 0, 0, newWidth, newHeight, null);
+
+            // Convert the resized image to ASCII characters
+            StringBuilder sb = new StringBuilder();
+            String characters = "@#S%?*+;:,.";// ASCII characters from darkest to lightest
+            for (int y = 0; y < newHeight; y++) {
+                for (int x = 0; x < newWidth; x++) {
+                    int rgb = resizedImage.getRGB(x, y);
+                    int gray = (int) (0.2126 * ((rgb >> 16) & 0xff) + 0.7152 * ((rgb >> 8) & 0xff) + 0.0722 * (rgb & 0xff));
+                    int index = (int) (gray / (255.0 / (characters.length() - 1)));
+                    sb.append(characters.charAt(index));
+                }
+                sb.append("\n");
+            }
+
+            // Print the ASCII art
+            System.out.println(sb.toString());
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
+
+
+
+
+
+
